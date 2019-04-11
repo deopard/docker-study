adff0a2e21bdffbcf01696ec912b5d0de74f0157
myoungho.pak <qkraudghgh@gmail.com>
2019-04-11 11:23:22 +0900

    200을 반환하는 healthcheck용 route 생성

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'item/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/health', to: ->(_) { [200, {}, ['']] }
 end
